fbc79da1df655f192fef0d60298212766cc9221b
Ruthika Pyreddy <your-email@example.com>
2026-07-27 23:11:12 -0700

    Configure settings for Render deployment

--- custom_search_engine/settings.py
+++ custom_search_engine/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [
     ".onrender.com",
 ]
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
